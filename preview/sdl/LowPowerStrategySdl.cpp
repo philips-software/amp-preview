69a0277d72c17a92fe39ede916849d4eeaa599db
Richard Peters <richard.peters@philips.com>
2026-01-28 14:48:56 +0100

    chore: update sdl from v2.26.2 to v3.4.0

--- preview/sdl/LowPowerStrategySdl.cpp
+++ preview/sdl/LowPowerStrategySdl.cpp
@@ -1,5 +1,5 @@
 #include "preview/sdl/LowPowerStrategySdl.hpp"
-#include "SDL.h"
+#include "SDL3/SDL.h"
 
 namespace services
 {
@@ -16,9 +16,9 @@ namespace services
         if (SDL_WaitEventTimeout(&event, static_cast<int>(std::min<uint64_t>(std::max<int64_t>(std::chrono::duration_cast<std::chrono::milliseconds>(timerService.NextTrigger() - timerService.Now()).count(), 0), 10000))) != 0)
             switch (event.type)
             {
-                case SDL_QUIT:
+                case SDL_EVENT_QUIT:
                     std::exit(0);
-                case SDL_MOUSEMOTION:
+                case SDL_EVENT_MOUSE_MOTION:
                 {
                     SDL_MouseMotionEvent& mouseEvent = reinterpret_cast<SDL_MouseMotionEvent&>(event);
                     NotifyObservers([&mouseEvent](SdlMouseObserver& observer)
@@ -27,7 +27,7 @@ namespace services
                         });
                     break;
                 }
-                case SDL_MOUSEBUTTONDOWN:
+                case SDL_EVENT_MOUSE_BUTTON_DOWN:
                 {
                     SDL_MouseButtonEvent& mouseEvent = reinterpret_cast<SDL_MouseButtonEvent&>(event);
                     if (mouseEvent.button == 1)
@@ -37,7 +37,7 @@ namespace services
                             });
                     break;
                 }
-                case SDL_MOUSEBUTTONUP:
+                case SDL_EVENT_MOUSE_BUTTON_UP:
                 {
                     SDL_MouseButtonEvent& mouseEvent = reinterpret_cast<SDL_MouseButtonEvent&>(event);
                     if (mouseEvent.button == 1)
